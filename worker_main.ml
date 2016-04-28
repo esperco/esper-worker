8e41581006b9e50676bd2cb9b2a688c719cb5f19
Martin Jambon <martin@mjambon.com>
2016-04-27 19:17:00 -0700

    Work in progress

---
+++ worker_main.ml
@@ -0,0 +1,28 @@
+(*
+   Usually this program should be linked with -linkall to ensure
+   that the registration of the job handlers takes place
+   even if the modules containing the initialization code
+   are not referenced anywhere.
+*)
+
+open Lwt
+
+let pid_file = "/var/log/wolverine/worker.pid"
+
+(*
+   We check the process ID of the previous worker to determine whether
+   this worker should start.
+*)
+let main ~offset =
+  Cmdline.parse_options ~offset [];
+  let job =
+    Apputil_worker.previous_worker_is_still_running
+      ~max_age:600. pid_file >>= function
+    | true ->
+        Log.logf `Info "Previous worker is still running. Aborting.";
+        return ()
+    | false ->
+        Apputil_worker.save_pid pid_file;
+        Worker.run_all ()
+  in
+  Util_lwt_main.run job
